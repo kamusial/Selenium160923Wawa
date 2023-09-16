02dca564693567d92c8cfa3e5a1e70d4b9d964ff
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-16 10:19:37 +0200

    1

--- Selenium1.py
+++ Selenium1.py
@@ -5,10 +5,16 @@ driver = webdriver.Chrome()
 driver.get('https:/google.com')
 time.sleep(1)
 print('strona :',driver.title)
-button1_accept = driver.find_element('id','L2AGLb')
-# print(button1_accept)
-# print(type(button1_accept))
 
+button1_accept = driver.find_element('id','L2AGLb')
 button1_accept.click()
+#driver.find_element('id','L2AGLb').click()   #to co wyżej, w jednej linii
+
+search_field = driver.find_element('name', 'q')
+search_field.send_keys('Ile lat ma Anna Lewandowska?')
+
+search_button = driver.find_element('name', 'btnK')
+search_button.submit()
+
 time.sleep(1)
 driver.quit()
