3c964d2406094d64f845f2502545d9ddac090210
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-16 09:55:26 +0200

    1

--- Selenium1.py
+++ Selenium1.py
@@ -4,4 +4,11 @@ import time
 driver = webdriver.Chrome()
 driver.get('https:/google.com')
 time.sleep(1)
+print('strona :',driver.title)
+button1_accept = driver.find_element('id','L2AGLb')
+# print(button1_accept)
+# print(type(button1_accept))
+
+button1_accept.click()
+time.sleep(1)
 driver.quit()
