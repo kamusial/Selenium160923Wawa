9016afe5db74449228f84a3940473f2f62eae007
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-16 13:02:36 +0200

    1

---
+++ Selenium3.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as excon
+
+def czekaj_na_id(element_id):
+    timeout = 5
+    timeout_message = f'Element o ID {element_id} nie pojawił się w czasie {timeout} s.'
+    lokalizator = (By.ID, element_id)
+    znaleziono = excon.visibility_of_element_located(lokalizator)    #sprawdzamy, czy element jest
+    oczekiwator = WebDriverWait(driver, timeout)    #jak długo czekac i gdzie
+    return oczekiwator.until(znaleziono, timeout_message)    #zwrotka
+
+driver = webdriver.Chrome()
+driver.get('https//www.saucedemo.com/')
+
