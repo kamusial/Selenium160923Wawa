889efae4321387eea6e0408c3304ecafc5ca5d38
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-09-17 11:37:47 +0200

    1

--- Selenium6.py
+++ Selenium6.py
@@ -4,17 +4,24 @@ import time
 from Selenium2 import make_screenshot
 import pytest
 
-@pytest.mark.parametrize('username', ['standard_user', 'lockout_user'])
-def test_login_page(username):   #parametr
+test_data = [
+    ('standard_user', 'secret_sauce', 'https://www.saucedemo.com/inventory.html'),
+    ('locked_out_user', 'secret_sauce', 'https://www.saucedemo.com/'),
+    ('problem_user', 'secret_sauce', 'https://www.saucedemo.com/inventory.html'),
+    ('performance_glitch_user', 'secret_sauce', 'https://www.saucedemo.com/inventory.html')
+]
+
+@pytest.mark.parametrize('username, password, url', test_data)
+def test_login_page(username, password, url):   #parametr
     driver = webdriver.Chrome()
     page = LoginPage(driver)
     page.open()
     page.enter_username(username)   #parametr
-    page.enter_password('secret_sauce')
+    page.enter_password(password)
     page.click_login()
     time.sleep(1)
     try:
-        assert driver.current_url == 'https://www.saucedemo.com/inventory.html', make_screenshot(driver)
+        assert driver.current_url == url, make_screenshot(driver)
         #jesli asercja sie nie uda, zrob screenshota
     except AssertionError:
         print('blad, zly adres')
